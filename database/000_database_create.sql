dfc3e2399a0289f32605b6b8d2123ab262c0edb9
JoshGill3882 <joshgill2013@gmail.com>
2025-11-07 16:30:13 +0000

    No-longer needed DB scripts removed

--- database/000_database_create.sql
+++
@@ -1,5 +0,0 @@
--- Create a dedicated schema for application tables if it does not already exist.
-CREATE SCHEMA IF NOT EXISTS pulse;
-
--- Ensure objects in this script (and others that set the search path) target the pulse schema.
-SET search_path TO pulse, public;
